20924e00b3ec2597bbe2bf36c54b9f971e95d0b8
msaf1980 <msaf1980@gmail.com>
2021-07-21 16:19:08 +0500

    build: add install target

--- cmake_init/00-options.cmake
+++ cmake_init/00-options.cmake
@@ -1,3 +1,8 @@
 option(PROFILE "Profile with google perftools" OFF)
 option(COVERAGE "Coverage" OFF)
 option(ENABLE_VALGRIND "Tests under valgring (test-valgrind target)" ON)
+option(EXPORT_COMPILE "Export compile database" OFF)
+
+if(EXPORT_COMPILE)
+	set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+endif()
