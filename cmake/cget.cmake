9482e734ad493c01754c2bf791170d753be62eec
msaf1980 <msaf1980@gmail.com>
2021-07-30 13:21:29 +0500

    build: rename dependecies find script for cmake

--- cmake/cget.cmake
+++
@@ -1,6 +0,0 @@
-function(cget_install)
-    execute_process(
-        COMMAND cget install --build_type ${CMAKE_BUILD_TYPE} -B ${CMAKE_BINARY_DIR}
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    )
-endfunction ()
